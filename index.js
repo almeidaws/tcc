13b9454079026a1240f75725597e422e41c76bc3
Gustavo Almeida <g-gustavoalmeida@hotmail.com>
2019-01-30 20:40:43 -0200

    Changes application port

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
+app.get('/register', (request, response) => {
+    response.send('registered');
+});
+
+const database = require('./database.js');
+database.connect();
+
 //add the router
 app.use('/', router);
 app.use(express.static(__dirname + '/public'));
-app.listen(process.env.port || 3000);
+app.listen(process.env.port || 5000);
