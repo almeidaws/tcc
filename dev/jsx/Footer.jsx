66ceb48d6f0acd8e1220408f014b92c564a459bd
Gustavo Almeida <g-gustavoalmeida@hotmail.com>
2019-05-24 21:59:39 -0300

    Remove useless text from footer

--- dev/jsx/Footer.jsx
+++ dev/jsx/Footer.jsx
@@ -7,36 +7,6 @@ const Footer = props => {
                 <div className="ms_footer_logo">
                     <a href="./index.html"><img src="./images/open_logo.png" alt=""/></a>
                 </div>
-                <div className="ms_footer_inner">
-                    <div className="row">
-                        <div className="col-lg-3 col-md-6">
-                            <div className="footer_box">
-                                <h1 className="footer_title">miraculous music station</h1>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
-                                    duis aute irure dolor.</p>
-                            </div>
-                        </div>
-                        <div className="col-lg-3 col-md-6">
-                            <div className="footer_box footer_contacts">
-                                <h1 className="footer_title">contact us</h1>
-                                <ul className="foo_con_info">
-                                    <li>
-                                        <div className="foo_con_icon">
-                                            <img src="./images/svg/message.svg" alt=""/>
-                                        </div>
-                                        <div className="foo_con_data">
-                                            <span className="con-title">email us :</span>
-                                            <span><a href="#">demo@mail.com </a>, <a
-                                                href="#">dummy@mail.com</a></span>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
                 <div className="col-lg-12">
                     <div className="ms_copyright">
                         <div className="footer_border"/>
